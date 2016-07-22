0bde0b1d225740a7a655c5e5b769333b0e568bf9
Rahul Bonnerjee <Rahul.Bonnerjee@gmail.com>
2016-07-22 09:36:33 -0400

    Login Sesssion Work in Progress

--- src/main/java/com/howtodoinjava/demo/controller/LoginController.java
+++ src/main/java/com/howtodoinjava/demo/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.howtodoinjava.demo.controller;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
@@ -16,12 +17,13 @@ import net.squadleader.people.Person;
 @Controller
 public class LoginController {
 	@RequestMapping("/submitLogin")
-	public String submitLogin(@ModelAttribute("command") Person person, Model model, @CookieValue(value = "loggedIn", defaultValue="false") String loggedIn, HttpServletResponse response){
+	public String submitLogin(@ModelAttribute("command") Person person, Model model, @CookieValue(value = "loggedIn", defaultValue="false") String loggedIn,
+			HttpServletResponse response){
 		if (PeopleDAO.checkLogin(person)){
 			Cookie cookie = new Cookie("loggedIn","true");
 			cookie.setMaxAge(60);
 			response.addCookie(cookie);
-			model.addAttribute("cookie", cookie);
+			//model.addAttribute("cookie", cookie);
 			return "addSuccess";
 		} else {
 			if (PeopleDAO.containsPerson(person))
@@ -32,7 +34,46 @@ public class LoginController {
 		}
 	}
 	@RequestMapping("/createLogin")
-	public ModelAndView createLogin() {
+	public ModelAndView createLogin(HttpServletResponse response, @CookieValue(value="loggedIn", defaultValue="false")String cookie) {
+		System.out.println("cookie = " + cookie);
+		if(cookie.equalsIgnoreCase("false")){
+			return new ModelAndView("login", "command", new Person());
+		}else{
+			return new ModelAndView("addSuccess");
+		}
+	}
+	
+	@RequestMapping("/Logout")
+	public ModelAndView logout(HttpServletRequest request, HttpServletResponse response, @CookieValue(value="loggedIn")String cookie){
+		Cookie[] cookies = request.getCookies();
+		if(cookies != null) {
+			for (Cookie cook: cookies) {
+				if (cook.getName().equals("loggedIn"))
+					cook.setMaxAge(0);
+					cook.setValue("false");
+					response.addCookie(cook);
+			}
+		}
 		return new ModelAndView("login", "command", new Person());
 	}
+	
+	@RequestMapping("/addSuccess")
+	public ModelAndView addSuccess(HttpServletRequest request){
+	
+		String loggedIn = null;
+		Cookie[] cookies = request.getCookies();
+		if(cookies != null) {
+			for (Cookie cookie: cookies) {
+				if (cookie.getName().equals("loggedIn") && cookie.getValue().equals("true"))
+					loggedIn = cookie.getValue();
+				return new ModelAndView("addSuccess");
+			}
+		}
+		if(loggedIn == null)
+			System.out.println("bruh");
+		
+		return new ModelAndView("login", "command", new Person());
+		
+	}
+	
 }
