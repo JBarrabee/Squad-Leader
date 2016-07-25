78fcfd10eadd10a9efc8cd484ae59f7d9a6ba9b4
Jonpaul Barrabee <jpbarrabee@yahoo.com>
2016-07-25 16:29:27 -0400

    the meetUpMap page is working, trying to add a link

--- src/main/java/com/howtodoinjava/demo/controller/MeetupController.java
+++ src/main/java/com/howtodoinjava/demo/controller/MeetupController.java
@@ -16,6 +16,8 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -44,61 +46,82 @@ import antlr.collections.List;
 // This is what the index page needs to ask for in the URL
 
 public class MeetupController {
+	// @RequestParam(value = , )
+	// int include = ${selections.include}
 	int include = 0;
-	int limit = 10;
-	String keyWord = "electronics";
+	String keyWord = "The";
+	// String keyWord = "";// ${selections.keyword}
+	int limit = 50; // take out as soon as it works on the meetUpMap.
 
 	@RequestMapping("/meetUpMap")
 
 	// Creating the Model and View
-	public ModelAndView meetUpMap(Model model) throws FileNotFoundException, IOException, ParseException {
-
+	public ModelAndView meetUpMap(Model model, HttpServletRequest servelet)
+			throws FileNotFoundException, IOException, ParseException {
+		if (servelet.getParameter("include") != null) {
+			include = Integer.parseInt(servelet.getParameter("include"));
+		}
+		if (servelet.getParameter("keyWord") != null) {
+			keyWord = servelet.getParameter("keyWord");
+		}
+		if (servelet.getParameter("limit") != null) {
+			limit = Integer.parseInt(servelet.getParameter("limit"));
+		}
 		// Creating and filling the ArrayList, Going to put in if statements
 		ArrayList<String> URLList = new ArrayList<String>();
 		ArrayList<String> descriptionList = new ArrayList<String>();
 		if (include == 0 || include == 1) {
 			URLList.add(
 					"https://api.meetup.com/ITintheD/events?photo-host=public&page=20&sig_id=209133816&sig=f1f01a0c6faa4c32e47ee45454c2af4d850723de");
-		}
-		// Adding events to the list to be searched for a key word.
-		if (include == 0 || include == 1) {
 			descriptionList.add(
 					"https://api.meetup.com/ITintheD?photo-host=public&sig_id=209133816&only=description&sig=1305e66cfaa3247e94c63d45d598a7747b709648");
 		}
 		if (include == 0 || include == 2) {
 			URLList.add(
 					"https://api.meetup.com/refreshdetroit/events?photo-host=public&page=20&sig_id=9283563&sig=182244089349639c68879c73ba06b4d567a867c2");
-		}
-		if (include == 0 || include == 2) {
 			descriptionList.add(
 					"https://api.meetup.com/refreshDetroit?photo-host=public&sig_id=209133816&only=description&sig=ebfeca1f2389e7dc0ace9a2abedb529d6b2674fd");
 		}
 		if (include == 0 || include == 3) {
 			URLList.add(
 					"https://api.meetup.com/BDPA-Detroit-technology-meetup/events?photo-host=public&page=20&sig_id=209133816&sig=db30a53e119fe9a87011a882d0fb33f1d7120110");
+			descriptionList.add(
+					"https://api.meetup.com/BDPA-Detroit-technology-meetup?photo-host=public&sig_id=209133816&only=description&sig=94c044814bd1f0bfa5777949afd89c78524803b2");
 		}
 		if (include == 0 || include == 4) {
 			URLList.add(
 					"https://api.meetup.com/Detroit-java-user-group/events?photo-host=public&page=20&sig_id=209133816&sig=1267a5d24e1aac9d58c5f1648f096629783a9517");
+			descriptionList.add(
+					"https://api.meetup.com/Detroit-java-user-group?photo-host=public&sig_id=209133816&only=description&sig=9feccd227ca59d03c316ec8813c9de2ab271c7d9");
 		}
 		if (include == 0 || include == 5) {
 			URLList.add(
 					"https://api.meetup.com/girl-develop-it-detroit/events?photo-host=public&page=20&sig_id=209133816&sig=a531894ec6e4a1645afb57e9857d08a782203662");
+			descriptionList.add(
+					"https://api.meetup.com/girl-develop-it-detroit?photo-host=public&sig_id=209133816&only=description&sig=5cf899c36a636d58847350f3bf247f30ff6defef");
 		}
 		if (include == 0 || include == 6) {
 			URLList.add(
 					"https://api.meetup.com/Detroit-User-Experience/events?photo-host=public&page=20&sig_id=209133816&sig=2a2208d5ccbf840bae8af880d717b35569c3088b");
+			descriptionList.add(
+					"https://api.meetup.com/Detroit-User-Experience?photo-host=public&sig_id=209133816&only=description&sig=b750b6ba39bc412377710db9d49372fedbe74229");
 		}
 		if (include == 0 || include == 7) {
 			URLList.add(
 					"https://api.meetup.com/Great-Lakes-Area-NET-User-Group-MIGANG/events?photo-host=public&page=20&sig_id=209133816&sig=c4d9e411d883061af6ee32c29586206646adec6e");
+			descriptionList.add(
+					"https://api.meetup.com/Great-Lakes-Area-NET-User-Group-MIGANG?photo-host=public&sig_id=209133816&only=description&sig=a2d39d97426b8587735edebfabdb26baa910fd07");
 		}
 		if (include == 0 || include == 8) {
 			URLList.add(
 					"https://api.meetup.com/TechShopDetroit/events?photo-host=public&page=20&sig_id=209133816&sig=fb1007044b0b014743f8f3b2bdb8b79a62c9b028");
+			descriptionList.add(
+					"https://api.meetup.com/TechShopDetroit?photo-host=public&sig_id=209133816&only=description&sig=5a785f78615af7c06da0d6c4409a691597e4b523");
 		}
 		if (include == 0 || include == 9) {
 			URLList.add("https://api.meetup.com/Tech248-Meetup/events?&sign=true&photo-host=public&page=20");
+			descriptionList.add(
+					"https://api.meetup.com/Tech248-Meetup?photo-host=public&sig_id=209133816&only=description&sig=e9085e0debec464fbd506ff587c744440fdb3576");
 		}
 
 		// used at bottom Had to keep out of the for loop
@@ -166,21 +189,22 @@ public class MeetupController {
 
 			// LIMIT SIZE
 			// number just needs to be initialized
-			int number = 0;
-			if (objArray.size() < limit)
-				number = objArray.size();
-			else
-				number = limit;
+			// int number = 0;
+			// if ( < limit)
+			// number = objArray.size();
+			// else
+			// number = limit;
 			// Third array use c
-			for (int c = 0; c < number; c++) {
+			for (int c = 0; c < objArray.size(); c++) {
 				Meetup meetup = new Meetup();
 				JsonObject eventObject = objArray.get(c).getAsJsonObject();
 				JsonObject group = eventObject.get("group").getAsJsonObject();
 				String groupURL = group.get("urlname").getAsString();
 				String groupName = group.get("name").getAsString();
 				String eventName = eventObject.get("name").getAsString();
-
+				String eventLink = eventObject.get("link").getAsString();
 				String eventDateString = eventObject.get("time").getAsString();
+
 				String shortString = eventDateString.substring(0, eventDateString.length() - 4);
 				Long eventDateLong = Long.parseLong(eventDateString);
 
@@ -207,6 +231,7 @@ public class MeetupController {
 				meetup.setGROUP_URL(groupURL);
 				meetup.setGROUP_NAME(groupName);
 				meetup.setEVENT_NAME(eventName);
+				meetup.setEVENT_LINK(eventLink);
 				meetup.setEVENT_DATE(formattedDate);
 				meetup.setEVENT_DATE_INT(eventDateInt);
 				meetup.setEVENT_VENUE_NAME(venueName);
@@ -248,26 +273,8 @@ public class MeetupController {
 		}
 		// This is where the eventList is packaged for sending
 		model.addAttribute("EventList", eventArray);
+		model.addAttribute("limit", limit);
 		return new ModelAndView("meetUpMap");
-	}
-
-	// Bellow is what is necessary to run the tests.
-	public static void main(String[] args) {
-		Model a = new BindingAwareModelMap();
-		MeetupController s = new MeetupController();
-		try {
-			s.meetUpMap(a);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
 	}
 
 }
--- src/main/webapp/WEB-INF/views/meetUpMap.jsp
+++ src/main/webapp/WEB-INF/views/meetUpMap.jsp
@@ -1,15 +1,18 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
- 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet"
+	href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script
+	src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
 <link rel="stylesheet" type="text/css"
 	href="bootstrap/css/bootstrap.css">
@@ -19,71 +22,72 @@
 <link rel="stylesheet" type="text/css" href="css/tablepage.css">
 <title>Upcoming Meetups</title>
 
- <style>
-       #map {
-        width: 100%;
-        height: 400px;
-      }
-    </style>
-   
+<style>
+#meetupDisplay {
+	width: 100%;
+	height: 400px;
+	text-align: left;
+}
+
+table, tr {
+	border: 1px solid black;
+	allign: left
+}
+
+th {
+	text-align: left;
+}
+</style>
+
 
 </head>
 
 <body>
-<div class="container">
-  <h2>Upcoming Meetups</h2>
-  
-  <table class="table table-hover">
-    <thead>
-      <tr>
- <script>
- 
- //${EventList}
- /* for (i = 0; i < EventList.length; i++) {
-	 
-		
-	 
- document.getElementById(list).innerHTML = document
-	.getElementById(list).innerHTML
-	+ "<p>"
-	+ "Group EVENT = " + EventList.get(i).meetup.EVENT_ID */
-	/* + ", Group URL = " + meetup.GROUP_URL
-	+ "<br>"
-	+ "Group Name = " + meetup.GROUP_NAME
-	+ ", Event Date =  " + meetup.EVENT_DATE
-	+ ", Event Name =  " + meetup.EVENT_NAME
-	+ "<br>"
-	+ "Event Venue Name = " + meetup.EVENT_VENUE_NAME
-	+ "<br>"
-	+ ", Event Street =  " + meetup.EVENT_STREET
-	+ ", Event City =  " + meetup.EVENT_CITY
-	+ ", Event State = " + meetup.EVENT_STATE
-	+ ", Event ZIP =  " + meetup.EVENT_ZIP
-	+ "<br>"
-	+ ", Event LONG = " + meetup.EVENT_LONGITUDE
-	+ ", Event LAT =  " + meetup.EVENT_LATITUDE
-	+ ", Event Link =  " + meetup.EVENT_LINK */
-	/* + "</p><br>";
- }
-      */
-    </script>
-<c:forEach var="meetup" items="${EventList}">
 
-<table>
-  <tr>
-				<td>${meetup.GROUP_NAME}</td>
-				<td>${meetup.EVENT_NAME}</td>
-				<td>${meetup.EVENT_DATE}</td>
-				<td>${meetup.EVENT_STREET}</td>
+	<h2>Upcoming Meetups</h2>
+
+	<form method="Post" action="meetUpMap.html">
+		<select name="limit">
+			<option value="2">3</option>
+			<option value="4">5</option>
+			<option value="29">30</option>
+		</select> <select name="include">
+			<option value="0">All</option>
+			<option value="1">IT in the D</option>
+			<option value="2">Refresh Detroit</option>
+			<option value="3">BDPA Detroit Technology</option>
+			<option value="4">Detroit Java User Group</option>
+			<option value="5">Girl DevelopIT Detroit</option>
+			<option value="6">Detroit User Experience</option>
+			<option value="7">Great Lakes Area NET User Group</option>
+			<option value="8">TechShop Detroit</option>
+			<option value="9">Tech248</option>
+		</select> <input type="text" name="keyWord"> <input type="submit"
+			value="Submit" />
+	</form>
+	<!--
+		<form action="#" th:action="@{/MeetupController}" th:object="${selections}" method="post">
+    	<p>Meetup Choice<input type="text" th:field="*{include}" /></p>
+        <p>keyword: <input type="text" th:field="*{keyWord}" /></p>
+        <p> <input type="reset" value="Reset" /></p>
+    </form>
+-->
+
+	<table id="MeetupDisplay" align="left">
+		<c:forEach var="meetup" begin="0" end="${limit}" items="${EventList}">
+			
+			<a href="${meetup.EVENT_LINK}"></a>
+				<tr >
+					<td>${meetup.GROUP_NAME}</td>
+					<td>${meetup.EVENT_NAME}</td>
+					<td>${meetup.EVENT_DATE}</td>
+					<td>${meetup.EVENT_CITY}</td>
 				</tr>
-				</tbody>
+	
+		</c:forEach>
 	</table>
-	 			
-</div>
-</c:forEach>
-    
-<%--  ${EventList } --%>
-   <div id= list> </div>
 
+
+	<div id=list></div>
 </body>
 </html>
